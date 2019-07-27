0b9d237d79a54005eb82a8c56d45843c0886e9ab
Samarth Patel <samarth.patel.22@hotmail.com>
2019-07-27 16:29:29 -0400

    Added socket and threading

---
+++ Tello.py
@@ -0,0 +1,30 @@
+import threading
+import socket
+import time
+
+class Tello:
+
+    PORT = 8889
+    HOST = ''
+
+    def __init__(self):
+        self.address = ('192.168.10.1', 8889)
+        self.response = None
+        
+        # UDP socket
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind((HOST, PORT))
+
+        # Thread for UDP reveicer 
+        recvThread = threading.Thread(target=receive)
+        recvThread.daemon = True # Thread ends when program ends
+        recvThread.start()
+
+    def receive(self):
+        while True:
+            try:
+                self.response, server = sock.recvfrom(1024)
+                print((data.decode(encoding='utf-8'), server))
+            except Exception as e:
+                print('Error: ' + e)
+                break
